e6bcc21bee6f38f7f956983e6b9e33f45b77b5f0
trollface11 <85368850+trollface11@users.noreply.github.com>
2021-07-18 19:16:31 -0400

    Add files via upload

--- Poker/pokerwrapper.py
+++ Poker/pokerwrapper.py
@@ -1,75 +1,38 @@
-from Poker.player import Player
-from Poker.pokerplayer import PokerPlayer
-from Poker.card import Card
-from Poker.announcer import Announcer
-import asyncio
-import discord
-
+from player import Player
+from card import Card
+from deck import Deck
+from announcer import Announcer
+from server import Server
+from leaderboard import Leaderboard
+from pokerplayer import PokerPlayer
 class PokerWrapper:
     def __init__(self):
-        self.gameID
+        self.gameID=0
         self.gameStarted=False
         self.numPlayers=0
         self.hardBlind=0
         self.currentPot=0
-        self.pokerUI
-        self.gamedeck
-        self.communityDeck
-        self.participants = []
-        self.startingBalance
-    
-    async def startGame(self, ctx):
-        await Announcer.initiateGame(ctx)
-    
-    async def setPlayers(self, ctx, bot):
-        embed = discord.Embed(title="Poker: Texas hold 'em", 
-        description="Starting Balance: "+str(self.startingBalance)+""" <:chips:865450470671646760>
-        Min Bet: """+str(self.hardBlind)+""" <:chips:865450470671646760> 
-        \nReact to Join!""",
-        color=discord.Color.green())
+        self.pokerUI=Announcer()
+        self.gamedeck=Deck()
+        self.communityDeck=[]
+        self.participants=[]
 
-        message = await ctx.send(embed=embed)
-        await message.add_reaction('✅')
-        await asyncio.sleep(10)
 
-        message = await ctx.fetch_message(message.id)
+    def removePlayer(self,id):
+        for i in self.participants:
+            if i.username()==id:
+                self.participants.remove(i)
+        self.numPlayers-=1
 
-        for reaction in message.reactions:
-            if reaction.emoji == '✅':
-                i = 1
-                async for user in reaction.users():
-                    if user != bot.user:
-                        self.participants.append(PokerPlayer(user.id, i))
-                        i += 1
-        if i < 3:
-            await ctx.send("Not enough players")
-            return False
-        else:
-            await ctx.send("Starting game with " + str(i) + " players")
-            
+    def addplayer(self,player):
+        self.participants.append(player)
+        self.numPlayers+=1
+    def printparticipants(self):
+        for i in self.participants:
+            print(i.username(),i.getGameBalance())
 
-    async def setBlind(self, ctx):
-        
-        def representsInt(s):
-            try: 
-                int(s)
-                return True
-            except ValueError:
-                return False
-        
-        def verify(m):
-            return m.author == ctx.message.author and representsInt(m.content)
-
-        await Announcer.askBet(ctx)
-
-        try:
-            msg = await self.client.wait_for('message', check = verify, timeout = 30)
-        except asyncio.TimeoutError:
-            await ctx.send(f"Sorry, you took too long to type the blind")
-            return False
-        
-        self.hardBlind = int(msg.content)
-
-        #await Announcer.reportBet(ctx, blind)
-        
-
+    def checkPlayerBalance(self):
+        for i in self.participants:
+            if i.getGameBalance()<=0:
+                print(i.username(), "has left the table")
+                self.participants.remove(i)
