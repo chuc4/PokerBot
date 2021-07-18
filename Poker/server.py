bb6f7dd637282e9326123e3455723a20f665d4b0
carrir2 <72038264+carrir2@users.noreply.github.com>
2021-07-18 17:14:40 -0400

    Embed update

--- Poker/server.py
+++ Poker/server.py
@@ -1,41 +1,67 @@
+from Poker.leaderboard import Leaderboard
+import discord
 from Poker.player import Player
 
 class Server:
-    def __init__(self):
+    def __init__(self, bot):
+        self.bot=bot
         self.players = {}
         self.resets = 0
     
-    def addPlayer(self, ctx):
+    async def addPlayer(self, ctx):
         if ctx.author.id not in self.players:
-            player = Player(ctx.author.name, 3000)
+            player = Player(ctx.author.id, 3000)
             self.players[ctx.author.id] = player
+            await ctx.send("You have created an account!")
             return True
         else:
+            await ctx.send("You already created an account!")
             return False
     
     async def getBalance(self, ctx):
         if ctx.author.id not in self.players:
             await ctx.send("You do not have an account! Use the .create command to create an account!")
-            return None
         else:
             player = self.players[ctx.author.id]
-            val=player.getBalance()
-            return val
-            # await ctx.send("You currently have " + str(player.getBalance()) + " chips!")
+            balance=player.getBalance()
+            embed = discord.Embed(title=ctx.author.name+"'s Balance:", 
+            description=str(balance)+" <:chips:865450470671646760>",
+            color=discord.Color.green())
+            embed.set_thumbnail(url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+
     
     def reset(self):
         for player in self.players:
             player.setBalance(3000)
         self.resets += 1
     
-    def help(self):
-        
+    async def help(self, ctx):
+
+        embed = discord.Embed(title="List of Commands", 
         description="""**.create** - Create your profile for the server 
         **.p** - Create and play a game of Texas Hold'Em Poker
         **.balance** - Check to see how many chips you have
         **.top** - Check the leaderboards to see who is on top
         **.join** - Join an already existing Poker game
-        **(Mods Only) .reset** - Reset the balances of everyone in the server"""
-        return description
+        **(Mods Only) .reset** - Reset the balances of everyone in the server""",
+        color=0xffffff)
+        embed.set_thumbnail(url="https://s.wsj.net/public/resources/images/JR-AA451_IFPOKE_GR_20191031164807.jpg")
+        await ctx.send(embed=embed)
+        
+    async def printLeaderboard(self, ctx):
+        sort_byvalue=sorted(self.players.items(),key=lambda x:x[1].balance,reverse=True)
+        leaderboard=""
+        k=1
+        for i in sort_byvalue:
+            name = str(await self.bot.fetch_user(i[1].id))
+            leaderboard+= str(k)+". "+name+" - "+ str(i[1].balance)+" <:chips:865450470671646760>\n"
+            k+=1
+        embed = discord.Embed(title=ctx.message.guild.name+" Leaderboard:",
+        description=leaderboard,
+        color=discord.Color.red())
+        embed.set_thumbnail(url=ctx.message.guild.icon_url) 
+        await ctx.send(embed=embed)
+
         
         
--- pokerbot.py
+++ pokerbot.py
@@ -19,7 +19,6 @@ def main():
         bot.run('ODYzMjAzODY1MjQzNjgwNzg4.YOjfPw.UKKwd0qMwzIk2dPgwZyXx4Eubxk')
     finally:
         print(f'End running at {time.asctime(time.localtime(time.time()))}')
-        # print("hi")
 
 @bot.event
 async def on_ready():
@@ -28,87 +27,35 @@ async def on_ready():
     print(bot.user.id)
     print('---------')
 
-server_bot = Server()
-leaderboard_bot = Leaderboard()
+server_bot = Server(bot)
+# leaderboard_bot = Leaderboard(bot)
 
 
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="List of Commands", 
-    description=server_bot.help(),
-    color=0xffffff)
-    embed.set_thumbnail(url="https://s.wsj.net/public/resources/images/JR-AA451_IFPOKE_GR_20191031164807.jpg")
-    await ctx.send(embed=embed)
+    await server_bot.help(ctx)
     
 
 @bot.command()
 async def create(ctx):
-    if (server_bot.addPlayer(ctx)): 
-        await ctx.send("You have created an account!")
-        leaderboard_bot.addplayer(str(ctx.author.id))
-        # leaderboard_bot.addplayer(str(str(ctx.author.name) + "#" + str(ctx.author.discriminator)))
-    else: await ctx.send("You already created an account!")
+    await server_bot.addPlayer(ctx)
+        
+
 
 @bot.command()
 async def top(ctx):
-    sort_byvalue= leaderboard_bot.getLeaderboard(ctx)
-    leaderboard=""
-    k=1
-    for i in sort_byvalue:
-        name = str(await bot.fetch_user(int(i[0])))
-        leaderboard+= str(k)+". "+name+" - "+ str(i[1])+" <:chips:865450470671646760>\n"
-        k+=1
-    embed = discord.Embed(title=ctx.message.guild.name+" Leaderboard:",
-    description=leaderboard,
-    color=discord.Color.red())
-    embed.set_thumbnail(url=ctx.message.guild.icon_url) 
-    await ctx.send(embed=embed)
-
+    await server_bot.printLeaderboard(ctx)
 
 
 @bot.command()
 async def balance(ctx):
-    balance= await server_bot.getBalance(ctx)
-    if (balance!=None):
-        embed = discord.Embed(title=ctx.author.name+"'s Balance:", 
-        description=str(balance)+" <:chips:865450470671646760>",
-        color=discord.Color.green())
-        embed.set_thumbnail(url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
+    await server_bot.getBalance(ctx)
 
 
+# @bot.command() 
+# async def p(ctx, *args):
 
-@bot.command() 
-async def p(ctx, *args):
-    blind = 20
-    startAmt = 1000
-
-    if len(args)==2:
-        blind = args[0]
-        startAmt = args[1]
-    
-    embed = discord.Embed(title="Poker: Texas hold 'em", 
-    description="Starting Balance: "+str(startAmt)+""" <:chips:865450470671646760>
-    Min Bet: """+str(blind)+""" <:chips:865450470671646760> 
-    \nReact to Join!""",
-    color=discord.Color.green())
-    players = []
-
-    message = await ctx.send(embed=embed)
-    await message.add_reaction('✅')
-    await asyncio.sleep(10)
-
-    message = await ctx.fetch_message(message.id)
-
-    for reaction in message.reactions:
-        if reaction.emoji == '✅':
-            async for user in reaction.users():
-                if user != bot.user:
-                    players.append(user.mention)
-    if len(players)<2:
-        await ctx.send("Not enough players")
-    else:
-        await ctx.send(players)
+        
         
 
 
